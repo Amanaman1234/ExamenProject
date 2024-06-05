3e868ec28846567ad8ca36a32ef88a605381afb3
Amanaman1234 <a.rampartaap@gmail.com>
2024-06-05 11:51:13 +0200

    ola

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php include("header.php") ?>
+<link rel="stylesheet" href="css/index.css">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>voedselpaket</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ registreer.php
@@ -0,0 +1,26 @@
+<?php include("header.php") ?>
+<link rel="stylesheet" href="css/registreer.css">
+
+
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registreer</title>
+</head>
+<body> 
+    <main class="main">
+        <section class="form">
+           <section class="links">
+            <h1>Registreer hier :</h1>
+
+            <form>
+                <input type="text" name="Naam" placeholder="Naam">
+            </form>
+           </section>
+           <section class="rechts"></section>
+        </section>
+    </main>
+</body>
+</html>
+
