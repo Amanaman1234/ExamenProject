568a4264faae687c2e048daac126584f813d6161
Mika <2174418@roc-dev.com>
2024-06-12 19:17:07 +0200

    header uit login en registreer gehaald
    het punt van inloggen is dat ze pas de klanten en alle info zien na het inloggen

--- invetaris.php
+++ invetaris.php
@@ -50,7 +50,7 @@
         $username = "root";
         $password = "";
         $dbname = "examenvoedselbank";
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname, 3307);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
