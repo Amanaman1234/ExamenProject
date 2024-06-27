5cd848254416f92c16868612eb8841558dd102f5
DannyVzU <112854038+DannyVzU@users.noreply.github.com>
2024-06-27 11:48:23 +0200

    3307 weg ik moet commiten anders mag ik niet pullen
    add product geeft de goede bar

--- invetaris.php
+++ invetaris.php
@@ -15,7 +15,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "examenvoedselbank";
-$conn = new mysqli($servername, $username, $password, $dbname, 3307);
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
